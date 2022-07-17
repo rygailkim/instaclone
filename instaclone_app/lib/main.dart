ff714d569662087496288a5f9ecb8cef33afc1e5
rygailkim@gmail.com <rygailkim@gmail.com>
2022-07-17 19:36:12 +0800

    setup firebase

--- instaclone_app/lib/main.dart
+++ instaclone_app/lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instaclone_app/responsive/mobile_screen_layout.dart';
 import 'package:instaclone_app/responsive/responsive_layout_screen.dart';
 import 'package:instaclone_app/responsive/web_screen_layout.dart';
 import 'package:instaclone_app/utils/colors.dart';
 
-void main() {
+void main() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyBMebs3TNOffcL3GnLFJ8G7nh9Q2GaRpvw",
+            authDomain: "instaclone-89523.firebaseapp.com",
+            projectId: "instaclone-89523",
+            storageBucket: "instaclone-89523.appspot.com",
+            messagingSenderId: "528672923573",
+            appId: "1:528672923573:web:183ddb199bbdd8b4a343fc"));
+  } else {
+    //mobile
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
